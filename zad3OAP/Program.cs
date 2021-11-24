c58b711daa345d97db8c8fd688cf588d5360f13d
vikto <vikto@DESKTOP-CBSCN3M>
2021-11-24 23:58:31 +0500

    Добавьте файлы проекта.

---
+++ zad3OAP/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace zad3OAP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("info.txt");
+            sw.WriteLine("Иванов Сергей Николаевич ПР-21 4,2");
+            sw.WriteLine("Петров Игорь Юрьевич ПР-21 3,5");
+            sw.WriteLine("Семёнов Михаил Алексеевич ПР-32 3,8");
+            sw.WriteLine("Пиманов Александр Дмитриевич ПР-32 4,1");
+            sw.WriteLine("Иванов Иван Петрович ПР-21 4,7");
+            sw.WriteLine("Усольцев Александр Дмитриевич ПР-32 4,3");
+            sw.Close();
+
+
+            StreamReader sr = new StreamReader("info.txt");
+            List<string[]> students = new List<string[]>();
+            while (!sr.EndOfStream)
+            {
+                string temp = sr.ReadLine();
+                students.Add(temp.Split(' '));
+            }
+
+            //var stdrework = students.Select(x => $"{string.Join(" ", x.Take(4))} {x[4].Replace(',', '.')}");
+            //Здесь я избавлялся от запятых и заменил их точками, но это не пригодилось :)
+
+            try
+            {
+                var sort = students.OrderBy(x => x[3]); //Сортировка по номеру группы
+                var min = students.Min(x => x[4]);
+                var max = students.Max(x => x[4]);
+                var avver = students.Average(x => double.Parse(x[4]));
+
+
+                Console.WriteLine($"Максимальный средний балл:  {max}");
+                Console.WriteLine($"Минимальный средний балл:  {min}");
+                Console.WriteLine($"Среднее значение средних баллов:  {Math.Round(avver, 3)} \n");
+
+                Console.WriteLine("Отсортированный лист:");
+                StreamWriter sw2 = new StreamWriter("rez.txt");
+                foreach (string[] z in sort)
+                {
+                    sw2.WriteLine(string.Join(" ", z));
+                    Console.WriteLine(string.Join(" ", z));
+                }
+                sw2.Close();
+            }
+            catch
+            {
+                Console.WriteLine("Неверные параметры, прочтенные из файла");
+            }
+
+
+
+        }
+    }
+}
---
+++ zad4OAP/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace zad4OAP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("info2.txt");
+            sw.WriteLine("Иванов Сергей Николаевич ПР-21 4,2");
+            sw.WriteLine("Петров Игорь Юрьевич ПР-21 3,5");
+            sw.WriteLine("Семёнов Михаил Алексеевич ПР-32 3,8");
+            sw.WriteLine("Пиманов Александр Дмитриевич ПР-32 4,1");
+            sw.WriteLine("Иванов Иван Петрович ПР-21 4,7");
+            sw.WriteLine("Усольцев Александр Дмитриевич ПР-21 4,3");
+            sw.Close();
+
+
+            StreamReader sr = new StreamReader("info2.txt");
+            List<string[]> students = new List<string[]>();
+            while (!sr.EndOfStream)
+            {
+                string temp = sr.ReadLine();
+                students.Add(temp.Split(' '));
+            }
+
+            //var gr2 = students.GroupBy(p => p[4]).Select(g => new { Name = g.Key, Count = g.Count(), Students = g.Select(p => p) });
+
+            try
+            {
+                var gr1 = students.GroupBy(g => g[3]).Select(g => new { name = g.Key, std = g.Where(x => x[4].StartsWith("4")), count = g.Count() - 1 });
+
+                foreach (var group in gr1)
+                {
+                    Console.WriteLine($"{group.name} : {group.count}");
+
+                    foreach (var x in group.std)
+                        Console.WriteLine(string.Join(" ", x));
+                    Console.WriteLine();
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Неверные входные параметры студента");
+            }
+
+
+        }
+    }
+}
